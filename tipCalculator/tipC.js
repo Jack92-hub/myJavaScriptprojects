5009bff51c7da937b7b6eec66037ba12eecbc4f2
Jack92-hub <jumrzokov@gmail.com>
2020-01-21 12:57:49 +0900

    added new files and modified file

---
+++ tipCalculator/tipC.js
@@ -0,0 +1,34 @@
+document.getElementById("calculate").addEventListener("click", function() {
+    let bill = document.getElementById("bill").value;
+    let serQual = document.getElementById("serQual").value;
+    let people = document.getElementById("people").value;
+    let result = bill * serQual / 100 / people;
+    if (bill =="") {
+        document.getElementById("afterbill").innerHTML = "Enter amount of the bill, please";
+        if (serQual =="") {
+            document.getElementById("afterselect").innerHTML = "Choose an option, please";
+            if (people ==""&& people == 0) {
+                document.getElementById("afterpeople").innerHTML = "Enter Number of People, please";
+            }
+        }
+        document.getElementById("bill").focus();
+    }else if (serQual =="") {
+        document.getElementById("afterbill").innerHTML = "";
+        document.getElementById("afterselect").innerHTML = "Choose an option, please";
+        if (people ==""&& people == 0) {
+            document.getElementById("afterpeople").innerHTML = "Enter Number of People, please";
+        }
+    }else if (people ==""&& people == 0) {
+        document.getElementById("afterbill").innerHTML = "";
+        document.getElementById("afterselect").innerHTML = "";
+        document.getElementById("afterpeople").innerHTML = "Enter Number of People, please";
+        document.getElementById("people").focus();
+    }else {
+        document.getElementById("afterbill").innerHTML = "";
+        document.getElementById("afterselect").innerHTML = "";
+        document.getElementById("afterpeople").innerHTML = "";
+        document.getElementById("h4").innerHTML = "TIP AMOUNT";
+        document.getElementById("amount").innerHTML = "$" + result.toFixed(2);
+        document.getElementById("each").innerHTML = "each";
+    }
+})
